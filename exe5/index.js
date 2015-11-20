ecf25bdfb840509bfcee221eaddd9fdb6e37bdd8
8108_web (Student) <s_8108_web@pedagogique.lan>
2015-11-20 09:37:41 +0100

    version 6 du projet

---
+++ exe5/index.js
@@ -0,0 +1,55 @@
+// recupère les infos derriere la commande node index.js
+// exemple: node index.js toto,titi,tutu,tete
+// et les écris dans un fichier list.txt les uns endessous des autres
+// et ajoutte à la suite à fois que l'on lance la commande
+
+// ma version
+// var fs = require("fs");
+// var args = process.argv.slice(2);
+//
+// args.forEach(function(val, index, array) {
+//   var name = val + '\n';
+//   console.log(name);
+//   if (index >=2);
+//     fs.appendFile('list.txt', name, function (err) {
+//       if (err) throw err;
+//       console.log('data was appended to file!');
+//   });
+// });
+
+
+// version prof (à preferer)
+var fs = require('fs');
+var utils = require('../modules/utils');
+var args = process.argv;
+var elements = null;
+(args[2])? elements = args[2].split(','):elements=[];
+fs.stat('list.txt', function(err, stat) {
+  if (err == null) {
+    console.log('File exist');
+    updateFile();
+  }
+  else if (err.code == 'ENOENT') {
+    console.log('File don\'t exists');
+    fs.writeFile('list.txt', '', function(err) {
+      if (err) throw err;
+      updateFile();
+  });
+
+  }
+  else {
+    console.log('Some other error: ', err.code);
+  }
+});
+
+function updateFile() {
+  fs.readFile('list.txt', function(err, data){
+    if (err) throw err;
+    var filedata = data.toString('utf8').split('\n');
+    var finalData = filedata.concat(elements);
+    console.log(finalData);
+    fs.writeFile('list.txt', utils.createNiceListofFiles(finalData), function(err) {
+      console.log("great done");
+    });
+  });
+}
